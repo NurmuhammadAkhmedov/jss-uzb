8f6e8edbd5274ec7b6eb535f9da04ea08c187083
NurmuhammadAkhmedov <ahmedov13nurmuhammad@gmail.com>
2025-01-24 16:45:06 +0500

    Add files via upload

---
+++ apple.js
@@ -0,0 +1,10 @@
+let name = prompt("ismingizni kiriting");
+let id = +prompt("Space idingizni kiriting");
+let password = +prompt("Space passwordingizni kiriting");
+let Info = {
+  name: name,
+  id: id,
+  password: password,
+};
+
+console.log(Info);
